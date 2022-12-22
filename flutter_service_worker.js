3eee6ecf4167c1bb14411866b869e631601268b2
engineerwr <37784533+engineerwr@users.noreply.github.com>
2022-12-22 15:31:29 -0300

    update inifinito

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "aae0c20084a3748628d90ab0814e67bd",
-"index.html": "c0be30898a2252ee3bb8ae7d522ffb13",
-"/": "c0be30898a2252ee3bb8ae7d522ffb13",
-"main.dart.js": "27168a49a75278cf5a55a8bc94f655e3",
+"index.html": "b58cc47120d63be838d3976e933f6dbe",
+"/": "b58cc47120d63be838d3976e933f6dbe",
+"main.dart.js": "9916d8ac9ccee50b0165317dcb75b364",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
